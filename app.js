683a2852a2f19ec555c1a2991be3470eeaafa92e
Ancelmo Cerqueira <aacn1@aluno.ifal.edu.br>
2021-08-30 15:11:24 -0300

    Renderizar os itens de tarefas

--- app.js
+++ app.js
@@ -8,17 +8,35 @@ function addTodo (Text) {
     };
 
     todoItemns.push(todo);
-    console.log(todoItemns);
+    renderTodo(todo);
 }
 
 const form = document.querySelector('.js-form');
 form.addEventListener("submit", event => {
+    event.preventDefault();
     const input = document.querySelector('.js-todo-input');
 
-    const text = input.nodeValue.trim();
+    const text = input.value.trim();
     if ( text !== "") {
         addTodo(text);
         input.value = '';
         input.focus();
     }
 });
+
+function renderTodo(todo) {
+    const list = document.querySelector(',js-todo-list');
+    const isChecked = document.checked ?  'done': '';
+    const node = document.createElement('li');
+    node.setAttribute('class', `todo-item ${isChecked}`);
+    node.setAttribute('data-key', todo.id);
+    node.innerHTML = 
+    ` <input id="${todo.id}" type="checkbox" />
+    <label for = "${todo.id}" class = "tick js-tick"> </label>
+    <span>${todo.text}</span>
+    <button class = "delee-todo js-delete-todo">
+    <svg> <use href="#delete-icon"></use></svg> </button> `
+    ;
+
+    list.append(node);
+}
