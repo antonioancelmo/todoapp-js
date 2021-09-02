d5544e14a9374b9c4dd781cb88d28ebc0788aa0f
Ancelmo Cerqueira <aacn1@aluno.ifal.edu.br>
2021-09-02 10:45:47 -0300

    Excluir itens de tarefas

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ function renderTodo(todo) {
   const list = document.querySelector('.js-todo-list');
   const item = document.querySelector(`[data-key='${todo.id}']`);
 
+  if (todo.deleted) {
+    item.remove();
+    return
+  }
+
   const isChecked = todo.checked ? 'done': '';
   const node = document.createElement("li");
   node.setAttribute('class', `todo-item ${isChecked}`);
@@ -41,6 +46,15 @@ function toggleDone(key) {
   renderTodo(todoItems[index]);
 }
 
+function deleteTodo(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    const todo = {
+      deleted: true,
+      ...todoItems[index]
+    };
+    todoItems = todoItems.filter(item => item.id !== Number(key));
+    renderTodo(todo);
+  }
 
 const form = document.querySelector('.js-form');
 form.addEventListener('submit', event => {
@@ -60,5 +74,11 @@ list.addEventListener('click', event => {
   if (event.target.classList.contains('js-tick')) {
     const itemKey = event.target.parentElement.dataset.key;
     toggleDone(itemKey);
+  }
+
+
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
   }
 });
